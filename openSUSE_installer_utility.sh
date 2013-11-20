75ba4386f9cc4b6f88533911ca6072dedf64376a
Rick dos Santos <rickpbusantos@gmail.com>
2013-11-20 02:03:47 -0200

    Como o nome já diz, é um programa de pos instalação do openSUSE 13.1, em que o usuário pode instalar todos os pacotes necessários para tornar o sistema utilizável no dia a dia
     "O Script deve ser executado como root"

--- openSUSE_installer_utility.sh
+++ openSUSE_installer_utility.sh
@@ -12,39 +12,39 @@ instalar=`cat pacotes.txt`
 
 (zenity --title="Atenção" --question --text="Você deseja prosseguir com a instalação dos pacotes $ler?") &&
  
-(zypper --no-gpg-checks --quiet ar http://ftp.gwdg.de/pub/linux/packman/suse/openSUSE_13.1/ "Packman" | zenity --title="Adicionado repositórios" --width="500" --height="200" --text="Adicionado repositório Principal" --progress --pulsate
-zypper --no-gpg-checks --quiet ar http://download.opensuse.org/repositories/server:/database/openSUSE_13.1/ "Jogos" | zenity --title="Adicionado repositórios" --width="500" --height="200" --text="Adicionado repositório Jogos" --progress --pulsate 
-zypper --no-gpg-checks --quiet ar  http://download.opensuse.org/repositories/multimedia:/libs/openSUSE_13.1/ "multmedia" | zenity --title="Adicionado repositórios" --width="500" --height="200" --text="Adicionado repositório Multimídia" --progress --pulsate
+(zypper --no-gpg-checks  ar http://ftp.gwdg.de/pub/linux/packman/suse/openSUSE_13.1/ "Packman" | zenity --title="Adicionado repositórios" --width="500" --height="200" --text="Adicionado repositório Principal" --progress --pulsate
+zypper --no-gpg-checks  ar http://download.opensuse.org/repositories/server:/database/openSUSE_13.1/ "Jogos" | zenity --title="Adicionado repositórios" --width="500" --height="200" --text="Adicionado repositório Jogos" --progress --pulsate 
+zypper --no-gpg-checks  ar  http://download.opensuse.org/repositories/multimedia:/libs/openSUSE_13.1/ "multmedia" | zenity --title="Adicionado repositórios" --width="500" --height="200" --text="Adicionado repositório Multimídia" --progress --pulsate
 
-zypper --no-gpg-checks --quiet ar  http://download.opensuse.org/repositories/graphics/openSUSE_13.1/ "Graphics" | zenity --title="Adicionado repositórios" --width="500" --height="200" --text="Adicionado repositório Gráficos..." --progress --pulsate 
+zypper --no-gpg-checks  ar  http://download.opensuse.org/repositories/graphics/openSUSE_13.1/ "Graphics" | zenity --title="Adicionado repositórios" --width="500" --height="200" --text="Adicionado repositório Gráficos..." --progress --pulsate 
 
-zypper --no-gpg-checks --quiet ar  http://download.opensuse.org/repositories/multimedia:/apps/openSUSE_13.1/ "av" | zenity --title="Adicionado repositórios" --width="500" --height="200" --text="Adicionado repositório Áudio e Video" --progress --pulsate --percentage="50"
+zypper --no-gpg-checks  ar  http://download.opensuse.org/repositories/multimedia:/apps/openSUSE_13.1/ "av" | zenity --title="Adicionado repositórios" --width="500" --height="200" --text="Adicionado repositório Áudio e Video" --progress --pulsate --percentage="50"
 
-zypper --no-gpg-checks --quiet ar  http://download.opensuse.org/repositories/KDE:/Extra/openSUSE_13.1/ "Extras" | zenity --title="Adicionado repositórios" --width="500" --height="200" --text="Adicionando repositorio Extras" --progress --pulsate
+zypper --no-gpg-checks  ar  http://download.opensuse.org/repositories/KDE:/Extra/openSUSE_13.1/ "Extras" | zenity --title="Adicionado repositórios" --width="500" --height="200" --text="Adicionando repositorio Extras" --progress --pulsate
 
-zypper --no-gpg-checks --quiet ar http://download.opensuse.org/repositories/network/openSUSE_13.1/ "network" | zenity --title="Adicionado repositórios" --width="500" --height="200" --text="Adicionando repositorio Network" --progress --pulsate
-
-
-zypper --no-gpg-checks --quiet ar http://download.opensuse.org/repositories/openSUSE:/13.1/standard/ "skype" | zenity --title="Adicionado repositórios" --width="500" --height="200" --text="Adicionando repositorio Network Extra" --progress --pulsate
+zypper --no-gpg-checks  ar http://download.opensuse.org/repositories/network/openSUSE_13.1/ "network" | zenity --title="Adicionado repositórios" --width="500" --height="200" --text="Adicionando repositorio Network" --progress --pulsate
 
 
-zypper --no-gpg-checks --quiet ar http://download.opensuse.org/repositories/home:/lijews:/branches:/OBS_Maintained:/opera/openSUSE_12.3_NonFree_Update/ "nonfree1" | zenity --title="Adicionado repositórios" --width="500" --height="200" --text="Adicionando repositorio NonFree" --progress --pulsate
+zypper --no-gpg-checks  ar http://download.opensuse.org/repositories/openSUSE:/13.1/standard/ "skype" | zenity --title="Adicionado repositórios" --width="500" --height="200" --text="Adicionando repositorio Network Extra" --progress --pulsate
 
-zypper --no-gpg-checks --quiet ar http://download.opensuse.org/repositories/M17N:/fonts/openSUSE_13.1/ "Fontes" | zenity --title="Adicionado repositórios" --width="500" --height="200" --text="Adicionando repositorio de Fontes" --progress --pulsate
 
-zypper --no-gpg-checks --quiet ar http://download.opensuse.org/repositories/home:/Simmphonie/openSUSE_13.1/ "av2" | zenity --title="Adicionado repositórios" --width="500" --height="200" --text="Adicionando repositorio Multimídia Extra" --progress --pulsate
+zypper --no-gpg-checks  ar http://download.opensuse.org/repositories/home:/lijews:/branches:/OBS_Maintained:/opera/openSUSE_12.3_NonFree_Update/ "nonfree1" | zenity --title="Adicionado repositórios" --width="500" --height="200" --text="Adicionando repositorio NonFree" --progress --pulsate
 
-zypper --no-gpg-checks --quiet ar http://download.opensuse.org/repositories/GNOME:/Apps/openSUSE_13.1/ "grafico" | zenity --title="Adicionado repositórios" --width="500" --height="100" --text="Adicionando repositorio Graficos Extra" --progress --pulsate
+zypper --no-gpg-checks  ar http://download.opensuse.org/repositories/M17N:/fonts/openSUSE_13.1/ "Fontes" | zenity --title="Adicionado repositórios" --width="500" --height="200" --text="Adicionando repositorio de Fontes" --progress --pulsate
 
-zypper --no-gpg-checks --quiet ar http://download.opensuse.org/repositories/GNOME:/Apps/openSUSE_13.1/ "pidgin" | zenity --title="Adicionado repositórios" --width="500" --height="100" --text="Adicionando repositorio Extra" --progress --pulsate
+zypper --no-gpg-checks  ar http://download.opensuse.org/repositories/home:/Simmphonie/openSUSE_13.1/ "av2" | zenity --title="Adicionado repositórios" --width="500" --height="200" --text="Adicionando repositorio Multimídia Extra" --progress --pulsate
 
-zypper --no-gpg-checks --quiet ar http://download.opensuse.org/repositories/network:/chromium/openSUSE_13.1/ "net2" | zenity --title="Adicionado repositórios" --width="500" --height="100" --text="Adicionando repositorio Extra" --progress --pulsate
+zypper --no-gpg-checks  ar http://download.opensuse.org/repositories/GNOME:/Apps/openSUSE_13.1/ "grafico" | zenity --title="Adicionado repositórios" --width="500" --height="100" --text="Adicionando repositorio Graficos Extra" --progress --pulsate
 
-zypper --no-gpg-checks --quiet ar http://download.opensuse.org/repositories/home:/RedDwarf/openSUSE_13.1/ "auda" | zenity --title="Adicionado repositórios" --width="500" --height="100" --text="Adicionando repositorio Extra" --progress --pulsate
+zypper --no-gpg-checks  ar http://download.opensuse.org/repositories/GNOME:/Apps/openSUSE_13.1/ "pidgin" | zenity --title="Adicionado repositórios" --width="500" --height="100" --text="Adicionando repositorio Extra" --progress --pulsate
 
-zypper --no-gpg-checks --quiet ar http://download.opensuse.org/repositories/utilities/openSUSE_13.1/ "utilidades" | zenity --title="Adicionado repositórios" --width="500" --height="100" --text="Adicionando repositorio Extra" --progress --pulsate
+zypper --no-gpg-checks  ar http://download.opensuse.org/repositories/network:/chromium/openSUSE_13.1/ "net2" | zenity --title="Adicionado repositórios" --width="500" --height="100" --text="Adicionando repositorio Extra" --progress --pulsate
 
- zypper --non-interactive --quiet install $instalar | zenity --title="Instalando" --width="500" --height="200" --title="Instalando pacotes" --text="Instalando pacotes aguarde..." --progress --pulsate
+zypper --no-gpg-checks  ar http://download.opensuse.org/repositories/home:/RedDwarf/openSUSE_13.1/ "auda" | zenity --title="Adicionado repositórios" --width="500" --height="100" --text="Adicionando repositorio Extra" --progress --pulsate
+
+zypper --no-gpg-checks  ar http://download.opensuse.org/repositories/utilities/openSUSE_13.1/ "utilidades" | zenity --title="Adicionado repositórios" --width="500" --height="100" --text="Adicionando repositorio Extra" --progress --pulsate
+
+ zypper --non-interactive install $instalar | zenity --title="Instalando" --width="500" --height="200" --title="Instalando pacotes" --text="Instalando pacotes aguarde..." --progress --pulsate
 
 
 
